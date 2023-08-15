359de3441f6f30913623539ef0a6c00313765377
Nick Munene <nickmunene101@gmail.com>
2023-08-15 22:00:07 +0300

    Removed styled component

--- src/components/ThemeManager.tsx
+++ src/components/ThemeManager.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import {StatusBar, StyleSheet} from 'react-native';
-import {ThemeProvider} from 'styled-components/native';
-
-import {light} from '../constants/theme';
 import LinearGradient from 'react-native-linear-gradient';
 
 export const ThemeManager = ({children}: {children: React.ReactNode}) => {
   return (
-    <ThemeProvider theme={light}>
+    <LinearGradient
+      style={styles.container}
+      colors={
+        true
+          ? ['#F8F8F8', '#C69F9B', '#180B2C']
+          : ['#6F225F', '#2F0E3C', '#180B2C']
+      }
+      start={{x: 0.2, y: 0.4}}>
       <StatusBar barStyle={'light-content'} />
-      <LinearGradient
-        style={styles.container}
-        colors={
-          true
-            ? ['#F8F8F8', '#C69F9B', '#180B2C']
-            : ['#6F225F', '#2F0E3C', '#180B2C']
-        }
-        start={{x: 0.2, y: 0.4}}>
-        {children}
-      </LinearGradient>
-    </ThemeProvider>
+      {children}
+    </LinearGradient>
   );
 };
 
