7e284d9c3686d98e423b6b3029d58890c810784b
Nick Munene <nickmunene101@gmail.com>
2023-08-16 15:01:42 +0300

    Typescript better usage

--- src/features/search/SearchPage.tsx
+++ src/features/search/SearchPage.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
-import {useSelector, useDispatch} from 'react-redux';
+import {useAppSelector, useAppDispatch} from '../../hooks/reduxHooks';
 
-import type {RootState} from '../../core/store';
 import {search} from './searchSlice';
 
 const SearchPage = () => {
-  const searchedCharacter = useSelector(
-    (state: RootState) => state.search.character,
-  );
-  const dispatch = useDispatch();
+  const searchedCharacter = useAppSelector(state => state.search.character);
+  const dispatch = useAppDispatch();
   return (
     <View className="flex pt-3">
       <Text className="text-center">{searchedCharacter}</Text>
