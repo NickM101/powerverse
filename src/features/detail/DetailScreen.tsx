fdd87266347100b118117b28e8468c7d4c7855bb
Nick Munene <nickmunene101@gmail.com>
2023-08-18 13:57:41 +0300

    Added multiple sheets

--- src/features/detail/DetailScreen.tsx
+++ src/features/detail/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, View, Button, Text, Dimensions} from 'react-native';
-import React, {useCallback, useMemo, useRef} from 'react';
+import React, {useCallback, useMemo, useRef, useEffect} from 'react';
 import FastImage from 'react-native-fast-image';
 import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
@@ -8,17 +8,38 @@ const {width, height} = Dimensions.get('window');
 const DetailScreen = () => {
   // ref
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const bottomSheetModalRef2 = useRef<BottomSheetModal>(null);
+  const bottomSheetModalRef3 = useRef<BottomSheetModal>(null);
+  const bottomSheetModalRef4 = useRef<BottomSheetModal>(null);
 
   // variables
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const snapPoints = useMemo(() => ['50%', '50%', '90%'], []);
+  const snapPoints2 = useMemo(() => ['40%', '40%', '80%'], []);
+  const snapPoints3 = useMemo(() => ['30%', '30%', '70%'], []);
+  const snapPoints4 = useMemo(() => ['20%', '20%', '50%'], []);
 
   // callbacks
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
+    bottomSheetModalRef2.current?.present();
+    bottomSheetModalRef3.current?.present();
+    bottomSheetModalRef4.current?.present();
   }, []);
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
+
+  useEffect(() => {
+    bottomSheetModalRef.current?.present();
+    bottomSheetModalRef2.current?.present();
+    bottomSheetModalRef3.current?.present();
+    bottomSheetModalRef4.current?.present();
+
+    return () => {};
+  }, [
+    bottomSheetModalRef,
+    bottomSheetModalRef2,
+    bottomSheetModalRef3,
+    bottomSheetModalRef4,
+  ]);
+
   return (
     <View style={styles.container}>
       <FastImage
@@ -40,10 +61,33 @@ const DetailScreen = () => {
             ref={bottomSheetModalRef}
             index={1}
             snapPoints={snapPoints}
-            onChange={handleSheetChanges}>
-            <View style={styles.contentContainer}>
-              <Text>Awesome 🎉</Text>
-            </View>
+            enablePanDownToClose={false}
+            closeOnOverlayTap={false}>
+            <Text>Awesome 1</Text>
+          </BottomSheetModal>
+          <BottomSheetModal
+            ref={bottomSheetModalRef2}
+            index={1}
+            snapPoints={snapPoints2}
+            enablePanDownToClose={false}
+            closeOnOverlayTap={false}>
+            <Text>Awesome 2</Text>
+          </BottomSheetModal>
+          <BottomSheetModal
+            ref={bottomSheetModalRef3}
+            index={1}
+            snapPoints={snapPoints3}
+            enablePanDownToClose={false}
+            closeOnOverlayTap={false}>
+            <Text>Awesome 3</Text>
+          </BottomSheetModal>
+          <BottomSheetModal
+            ref={bottomSheetModalRef4}
+            index={1}
+            snapPoints={snapPoints4}
+            enablePanDownToClose={false}
+            closeOnOverlayTap={false}>
+            <Text>Awesome 4</Text>
           </BottomSheetModal>
         </View>
       </BottomSheetModalProvider>
