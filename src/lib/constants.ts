dae250b6a8862644b8e8d54e0107073a9c248ea6
Nick Munene <nickmunene101@gmail.com>
2023-08-23 22:53:26 +0300

    Added publisher images

---
+++ src/lib/constants.ts
@@ -0,0 +1,27 @@
+export interface ComicPublishers {
+  name: string;
+  image: string;
+}
+
+export const publishers: ComicPublishers[] = [
+  {
+    name: 'Marvel Studio',
+    image: '',
+  },
+  {
+    name: 'DC Comics',
+    image: '',
+  },
+  {
+    name: 'Dark Horse Comics',
+    image: '',
+  },
+  {
+    name: 'Image Comics',
+    image: '',
+  },
+  {
+    name: 'Lucasfilm Studio',
+    image: '',
+  },
+];
