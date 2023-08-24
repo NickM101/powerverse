a5f12375bf2059e669b4b844b13023c47769c623
Nick Munene <nickmunene101@gmail.com>
2023-08-24 13:18:29 +0300

    setup of path alias

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [['module:react-native-dotenv'], 'react-native-reanimated/plugin'],
+  plugins: [
+    ['module:react-native-dotenv'],
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          images: ['./assets/images'],
+          components: ['./src/components'],
+          features: ['./src/features'],
+          hooks: ['./src/hooks'],
+          lib: ['./src/lib'],
+          navigation: ['./src/navigation'],
+          redux: ['./src/redux'],
+          screens: ['./src/screens'],
+          services: ['./src/services'],
+          theme: ['./src/theme'],
+          types: ['./src/types'],
+          utils: ['./src/utils'],
+        },
+      },
+    ],
+  ],
 };
